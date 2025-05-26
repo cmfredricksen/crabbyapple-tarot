e199f98ee86d0d11cc9580cec03b9a945cfbe7d0
Chris F <cmfredricksen@gmail.com>
2025-05-26 15:35:43 -0500

    add basic formatting

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -19,3 +19,19 @@
         {/each}
     </nav>
 </header>
+
+<style>
+    header {
+        padding: 2rem 1rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: baseline;
+    }
+    nav a{
+        margin: 0 1rem;
+        text-decoration: none;
+    }
+    a:hover {
+        color: grey;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script>
     import "../app.css"
-    import Header from "$lib/components/Header.svelte";
+    import { Header } from "$lib/components/index";
+    let { children } = $props()
 </script>
 
-<Header></Header>
+<Header />
 
 <main>
-    <slot />
+    {@render children()}
 </main>
+
+<style>
+    main {
+        width: 70%;
+        margin: 2rem auto;
+    }
+</style>
